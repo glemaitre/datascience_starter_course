b93999484c19dd495ac75e0ee05a5daf9a9ccef3
tomMoral <thomas.moreau.2010@gmail.com>
2020-10-28 10:15:10 +0100

    FIX sampling in 06/solution_4

--- 06_model_evaluation/solutions/solution_4.py
+++ 06_model_evaluation/solutions/solution_4.py
@@ -3,7 +3,9 @@ sample_sizes = [100, 500, 1000, 5000, 10000, 15000, y.size]
 scores_sample_sizes = {}
 rng = np.random.RandomState(0)
 for n_samples in sample_sizes:
-    sample_idx = rng.choice(np.arange(y.size), size=n_samples)
+    sample_idx = rng.choice(
+        np.arange(y.size), size=n_samples, replace=False
+    )
     X_sampled, y_sampled = X.iloc[sample_idx], y[sample_idx]
     size, score = make_cv_analysis(regressor, X_sampled, y_sampled)
     scores_sample_sizes[size] = score
